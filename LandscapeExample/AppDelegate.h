f3d9d176e2f51e9ebc5ef6683a2dad3b150af341
Jim Veneskey <github@strifecrafter.com>
2015-07-01 12:15:06 -0400

    initial commit

---
+++ LandscapeExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LandscapeExample
+//
+//  Created by jim Veneskey on 7/1/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
